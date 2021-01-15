c6596621e1b1771e40b4baef32ec10c0821e2a74
leewoongi <t2korea@naver.com>
2021-01-15 10:27:12 +0900

    feat:#7 diary UI 생성

---
+++ app/src/main/java/com/km/exchangediary/ui/diary/DiaryActivity.kt
@@ -0,0 +1,36 @@
+package com.km.exchangediary.ui.diary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.km.exchangediary.R
+import com.km.exchangediary.base.BaseActivity
+import com.km.exchangediary.databinding.ActivityDiaryBinding
+import com.km.exchangediary.ui.diary.adapter.DiaryViewPagerAdapter
+import com.km.exchangediary.uihelper.UiHelper
+import kotlinx.android.synthetic.main.activity_diary.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class DiaryActivity : BaseActivity<ActivityDiaryBinding>(){
+    private val viewModel: DiaryViewModel by viewModel()
+    override fun layoutRes(): Int = R.layout.activity_diary
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        UiHelper.toolBarInit(this, binding.diaryToolbar)
+
+        binding.diaryVm = viewModel
+        binding.diaryViewPager.adapter = DiaryViewPagerAdapter(supportFragmentManager,lifecycle)
+
+        makeDiary()
+    }
+
+    private fun makeDiary() {
+        with (binding.diaryViewPager.adapter as DiaryViewPagerAdapter) {
+            addFragment(arrayListOf(CoverFragment()))
+        }
+    }
+
+
+}
