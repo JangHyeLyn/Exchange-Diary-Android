f6e9c446b88e9fd3933c33e6405d027a3e6991c0
leewoongi <t2korea@naver.com>
2021-01-17 16:20:07 +0900

    refact#7 : 다이어리 페이지 toolbar 수정

--- app/src/main/java/com/km/exchangediary/ui/diary/DiaryActivity.kt
+++ app/src/main/java/com/km/exchangediary/ui/diary/DiaryActivity.kt
@@ -1,24 +1,21 @@
 package com.km.exchangediary.ui.diary
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
+import android.view.MotionEvent
 import com.km.exchangediary.R
 import com.km.exchangediary.base.BaseActivity
 import com.km.exchangediary.databinding.ActivityDiaryBinding
 import com.km.exchangediary.ui.diary.adapter.DiaryViewPagerAdapter
-import com.km.exchangediary.uihelper.UiHelper
-import kotlinx.android.synthetic.main.activity_diary.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DiaryActivity : BaseActivity<ActivityDiaryBinding>(){
     private val viewModel: DiaryViewModel by viewModel()
     override fun layoutRes(): Int = R.layout.activity_diary
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        UiHelper.toolBarInit(this, binding.tbDiary)
 
         binding.diaryVm = viewModel
         binding.vpDiary.adapter = DiaryViewPagerAdapter(supportFragmentManager, lifecycle)
@@ -26,5 +23,8 @@ class DiaryActivity : BaseActivity<ActivityDiaryBinding>(){
         with (binding.vpDiary.adapter as DiaryViewPagerAdapter) {
             addFragment(arrayListOf(CoverFragment()))
         }
+
+
     }
 }
+
