a24c0e3054254c94b4df805d16c1c3f15a3dc701
leewoongi <t2korea@naver.com>
2021-01-20 22:20:46 +0900

    feat: #7 코드리뷰 반영

--- app/src/main/java/com/km/exchangediary/ui/diary/DiaryActivity.kt
+++ app/src/main/java/com/km/exchangediary/ui/diary/DiaryActivity.kt
@@ -1,8 +1,6 @@
 package com.km.exchangediary.ui.diary
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.MotionEvent
 import com.km.exchangediary.R
 import com.km.exchangediary.base.BaseActivity
 import com.km.exchangediary.databinding.ActivityDiaryBinding
@@ -13,18 +11,14 @@ class DiaryActivity : BaseActivity<ActivityDiaryBinding>(){
     private val viewModel: DiaryViewModel by viewModel()
     override fun layoutRes(): Int = R.layout.activity_diary
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.diaryVm = viewModel
         binding.vpDiary.adapter = DiaryViewPagerAdapter(supportFragmentManager, lifecycle)
 
-        with (binding.vpDiary.adapter as DiaryViewPagerAdapter) {
+        with(binding.vpDiary.adapter as DiaryViewPagerAdapter) {
             addFragment(arrayListOf(CoverFragment()))
         }
-
-
     }
 }
-
