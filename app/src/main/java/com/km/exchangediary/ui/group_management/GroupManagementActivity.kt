bb96e383383dd4987afa8fc88099e1462e637867
KM <sodp2@naver.com>
2021-01-23 01:01:39 +0900

    feat: #11 그룹 관리 아이템 순서 변경 기능 추가

--- app/src/main/java/com/km/exchangediary/ui/group_management/GroupManagementActivity.kt
+++ app/src/main/java/com/km/exchangediary/ui/group_management/GroupManagementActivity.kt
@@ -1,13 +1,22 @@
 package com.km.exchangediary.ui.group_management
 
 import android.os.Bundle
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.km.exchangediary.R
 import com.km.exchangediary.base.BaseActivity
 import com.km.exchangediary.databinding.ActivityGroupManagementBinding
+import com.km.exchangediary.ui.group_management.adapter.GroupManagementAdapter
+import com.km.exchangediary.ui.group_management.adapter.GroupItemTouchHelperCallback
+
+interface GroupItemDragListener {
+    fun onDrag(holder: GroupManagementAdapter.GroupManagementViewHolder)
+}
 
 class GroupManagementActivity : BaseActivity<ActivityGroupManagementBinding>() {
     override fun layoutRes(): Int = R.layout.activity_group_management
+
+    private lateinit var touchHelper: ItemTouchHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +26,13 @@ class GroupManagementActivity : BaseActivity<ActivityGroupManagementBinding>() {
     private fun bindingView() {
         binding.rvGroupList.adapter = GroupManagementAdapter()
         binding.rvGroupList.layoutManager = LinearLayoutManager(this)
+
+        touchHelper = ItemTouchHelper(GroupItemTouchHelperCallback(binding.rvGroupList.adapter as GroupManagementAdapter))
+        (binding.rvGroupList.adapter as GroupManagementAdapter).setDragListener(object: GroupItemDragListener{
+            override fun onDrag(holder: GroupManagementAdapter.GroupManagementViewHolder) {
+                touchHelper.startDrag(holder)
+            }
+        })
+        touchHelper.attachToRecyclerView(binding.rvGroupList)
     }
 }
--- app/src/main/java/com/km/exchangediary/ui/group_management/adapter/GroupManagementAdapter.kt
+++ app/src/main/java/com/km/exchangediary/ui/group_management/adapter/GroupManagementAdapter.kt
@@ -1,16 +1,22 @@
-package com.km.exchangediary.ui.group_management
+package com.km.exchangediary.ui.group_management.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.km.exchangediary.R
 import com.km.exchangediary.databinding.ItemGroupManagementBinding
+import com.km.exchangediary.ui.group_management.GroupItemDragListener
 import com.km.exchangediary.ui.group_management.model.Group
+import java.util.*
 
-class GroupManagementAdapter : RecyclerView.Adapter<GroupManagementAdapter.GroupManagementViewHolder>() {
-    private val groupList = arrayListOf<Group>(
+class GroupManagementAdapter : RecyclerView.Adapter<GroupManagementAdapter.GroupManagementViewHolder>(), GroupItemMoveListener {
+    private var listener: GroupItemDragListener? = null
+
+    private val groupList = arrayListOf(
         Group("그룹 미지정", 1),
         Group("평균 28세들", 2),
         Group("그룹명최대열글자까지", 1),
@@ -51,10 +57,27 @@ class GroupManagementAdapter : RecyclerView.Adapter<GroupManagementAdapter.Group
         holder.onBind(groupList[position])
     }
 
-    class GroupManagementViewHolder(private val binding: ItemGroupManagementBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun setDragListener(listener: GroupItemDragListener) {
+        this.listener = listener
+    }
+
+    inner class GroupManagementViewHolder(private val binding: ItemGroupManagementBinding) : RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("ClickableViewAccessibility")
         fun onBind(item: Group) {
             binding.tvGroupName.text = item.name
             binding.tvNumberOfPeople.text = "(${item.numberOfPeople})"
+            binding.ivItemChanger.setOnTouchListener { _, event ->
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    listener?.onDrag(this)
+                }
+
+                return@setOnTouchListener true
+            }
         }
+    }
+
+    override fun onItemMove(fromPosition: Int, toPosition: Int) {
+        Collections.swap(groupList, fromPosition, toPosition)
+        notifyItemMoved(fromPosition, toPosition)
     }
 }
