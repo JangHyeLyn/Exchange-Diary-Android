93b0cbd2453f2b228dcb7927492ce13620e6702c
leewoongi <t2korea@naver.com>
2021-02-04 08:54:14 +0900

    feat: #14 툴바 이미지 추가 및 스위치 추가 및 클릭시 툴바 숨기기

--- app/src/main/java/com/km/exchangediary/ui/diary/DiaryActivity.kt
+++ app/src/main/java/com/km/exchangediary/ui/diary/DiaryActivity.kt
@@ -1,29 +1,23 @@
 package com.km.exchangediary.ui.diary
 
 import android.annotation.SuppressLint
-import android.gesture.GestureOverlayView
-import android.gesture.GestureOverlayView.OnGestureListener
 import android.os.Bundle
-import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.widget.SeekBar
-import android.widget.TextView
-import androidx.core.view.GestureDetectorCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.km.exchangediary.R
 import com.km.exchangediary.base.BaseActivity
 import com.km.exchangediary.databinding.ActivityDiaryBinding
 import com.km.exchangediary.ui.diary.adapter.DiaryViewPagerAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import timber.log.Timber
 
-class DiaryActivity: BaseActivity<ActivityDiaryBinding>() {
+class DiaryActivity: BaseActivity<ActivityDiaryBinding>(), GestureDetector.OnGestureListener {
     private val viewModel: DiaryViewModel by viewModel()
     override fun layoutRes(): Int = R.layout.activity_diary
 
-    var max: Int = 20
+    var max: Int = 40
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,93 +27,40 @@ class DiaryActivity: BaseActivity<ActivityDiaryBinding>() {
 
         binding.vpDiary.adapter = DiaryViewPagerAdapter(supportFragmentManager, lifecycle)
 
-        seekBarInit()
-
         with(binding.vpDiary.adapter as DiaryViewPagerAdapter) {
             addFragment(arrayListOf(CoverFragment(), CoverFragment(), CoverFragment(), CoverFragment(), CoverFragment(), CoverFragment(), CoverFragment()))
         }
 
-        //뷰페이저 움직이면 시크바 변경
-        binding.vpDiary.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                setSeekBarChange(max, 0, position, binding.tvTotalDiaryPage)
-            }
-        })
+        seekBarInit()
 
-
-        //시크바 움직이면 뷰페이져 같이 움직이기
-        binding.sbPageNumber.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                setSeekBarChange(max, 0, progress, binding.tvTotalDiaryPage)
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-
-        })
-
-        // 이벤트 재정의
-        var detector = GestureDetector(this, object:GestureDetector.OnGestureListener{
-            override fun onDown(p0: MotionEvent?): Boolean {
-                Log.d("test", "onDown")
-                return false
-            }
-
-            override fun onShowPress(p0: MotionEvent?) {
-                Log.d("test", "onShowPress")
-            }
-
-            override fun onSingleTapUp(p0: MotionEvent?): Boolean {
-                Log.d("test", "onSingleTapUp")
-
-                if (binding.sbContainer.visibility == View.VISIBLE) {
-                    binding.sbContainer.visibility = View.INVISIBLE
-                    binding.tbDiary.title = ""
-
-                } else {
-                    binding.sbContainer.visibility = View.VISIBLE
-                    binding.tbDiary.title = "최대열네글자를쓸수있게하고자"
-                }
-
-                return false
-            }
-
-            override fun onScroll(
-                    p0: MotionEvent?,
-                    p1: MotionEvent?,
-                    p2: Float,
-                    p3: Float
-            ): Boolean {
-                Log.d("test", "onScroll")
-                return false
-            }
-
-            override fun onLongPress(p0: MotionEvent?) {
-                Log.d("test", "onLongPress")
-            }
-
-            override fun onFling(
-                    p0: MotionEvent?,
-                    p1: MotionEvent?,
-                    p2: Float,
-                    p3: Float
-            ): Boolean {
-                Log.d("test", "onFling")
-                return false
-            }
-        })
-
+        var detector = GestureDetector(applicationContext,this)
 
         // 뷰페이저의 0번째 자식은 리사이클러뷰
         binding.vpDiary.getChildAt(0).setOnTouchListener{v, event->
             detector.onTouchEvent(event)
             false
         }
+
+        //뷰페이저 움직이면 시크바 변경
+        binding.vpDiary.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.sbPageNumber.progress = position
+                updatePage(position)
+            }
+        })
+
+        //시크바 움직이면 뷰페이져 같이 움직이기
+        binding.sbPageNumber.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.vpDiary.currentItem = progress
+                updatePage(progress)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
     }
 
     private fun seekBarInit() {
@@ -127,9 +68,48 @@ class DiaryActivity: BaseActivity<ActivityDiaryBinding>() {
         binding.tvTotalDiaryPage.text = "0 / $max"
     }
 
-    private fun setSeekBarChange(max: Int, min: Int, progress: Int, tv: TextView) {
-        var page: Int = min + (progress * 1)
-        binding.tvTotalDiaryPage.text = "$page / $max"
-        binding.vpDiary.currentItem = page
+    private fun updatePage(position: Int){
+        var page: String = "" + position
+        binding.tvTotalDiaryPage.text = "$page/ $max"
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        if (binding.sbContainer.visibility == View.VISIBLE) {
+            binding.sbContainer.visibility = View.INVISIBLE
+            binding.ivDiaryBack.visibility = View.INVISIBLE
+            binding.tvDiaryName.visibility = View.INVISIBLE
+            binding.ivDiaryText.visibility = View.INVISIBLE
+            binding.switchDiary.visibility = View.INVISIBLE
+            binding.ivDiaryImage.visibility = View.INVISIBLE
+            binding.btNextPage.visibility = View.INVISIBLE
+        } else {
+            binding.sbContainer.visibility = View.VISIBLE
+            binding.ivDiaryBack.visibility = View.VISIBLE
+            binding.tvDiaryName.visibility = View.VISIBLE
+            binding.ivDiaryText.visibility = View.VISIBLE
+            binding.switchDiary.visibility = View.VISIBLE
+            binding.ivDiaryImage.visibility = View.VISIBLE
+            binding.btNextPage.visibility = View.VISIBLE
+        }
+        return false
+
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+    }
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+        return false
     }
 }
