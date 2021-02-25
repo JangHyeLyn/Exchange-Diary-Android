899750255d8a8c88a5976b67bbe182167a09d254
hr955 <nfzoze01@gmail.com>
2021-02-25 13:58:21 +0900

    알림 item 생성

--- app/src/main/java/com/km/exchangediary/ui/bridge/BridgeActivity.kt
+++ app/src/main/java/com/km/exchangediary/ui/bridge/BridgeActivity.kt
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.km.exchangediary.R
 import com.km.exchangediary.base.BaseActivity
 import com.km.exchangediary.databinding.ActivityBridgeBinding
+import com.km.exchangediary.ui.notification_page.NotificationPageActivity
 import com.km.exchangediary.ui.home.HomeActivity
 import com.km.exchangediary.ui.diary.DiaryActivity
 import com.km.exchangediary.ui.main.MainActivity
@@ -20,7 +21,8 @@ class BridgeActivity : BaseActivity<ActivityBridgeBinding>() {
             bridgeAdapter.activityList = arrayListOf(
                     MainActivity::class.java,
                     HomeActivity::class.java,
-                    DiaryActivity::class.java
+                    DiaryActivity::class.java,
+                    NotificationPageActivity::class.java
             )
             adapter = bridgeAdapter
             layoutManager = LinearLayoutManager(this@BridgeActivity)
---
+++ app/src/main/java/com/km/exchangediary/ui/notification_page/NotificationPageActivity.kt
@@ -0,0 +1,16 @@
+package com.km.exchangediary.ui.notification_page
+
+import android.os.Bundle
+import com.km.exchangediary.R
+import com.km.exchangediary.base.BaseActivity
+import com.km.exchangediary.databinding.ActivityNotificationPageBinding
+
+
+class NotificationPageActivity : BaseActivity<ActivityNotificationPageBinding>() {
+    override fun layoutRes(): Int = R.layout.activity_notification_page
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
