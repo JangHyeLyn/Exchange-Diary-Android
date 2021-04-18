0f3b7e7cf3f8fff08c79026b6830120ba24e2146
KM <sodp2@naver.com>
2021-04-19 00:52:27 +0900

    feat: #30 로그인 UI 추가
    - LoginActivity에 있던 로직과 뷰모델을 LoginFragment로 이동

--- app/src/main/java/com/km/exchangediary/ui/login/adapter/OnBoardingViewPagerAdapter.kt
+++ app/src/main/java/com/km/exchangediary/ui/login/adapter/OnBoardingViewPagerAdapter.kt
@@ -4,13 +4,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.km.exchangediary.ui.login.LoginFragment
 import com.km.exchangediary.ui.login.OnBoardingFragment
 
 class OnBoardingViewPagerAdapter(fragmentManager: FragmentManager, lifeCycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifeCycle) {
     private val fragmentList = arrayListOf<Fragment>(
         OnBoardingFragment(0),
         OnBoardingFragment(1),
-        OnBoardingFragment(2)
+        OnBoardingFragment(2),
+        LoginFragment()
     )
 
     override fun getItemCount(): Int = fragmentList.size
