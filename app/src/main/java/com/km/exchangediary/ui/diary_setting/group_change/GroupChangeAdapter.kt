719b6041480c8284bfd8d28589d0dfc8e92e9f1e
hr955 <nfzoze01@gmail.com>
2021-07-09 22:05:23 +0900

    feat: #32 RecyclerView에서 RadioButton 단일선택 구현

--- app/src/main/java/com/km/exchangediary/ui/diary_setting/group_change/GroupChangeAdapter.kt
+++ app/src/main/java/com/km/exchangediary/ui/diary_setting/group_change/GroupChangeAdapter.kt
@@ -3,16 +3,13 @@ package com.km.exchangediary.ui.diary_setting.group_change
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.RadioButton
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.km.exchangediary.R
 import com.km.exchangediary.databinding.ItemGroupChangeRadioButtonBinding
-import kotlin.properties.Delegates
 
 class GroupChangeAdapter : RecyclerView.Adapter<GroupChangeAdapter.GroupChangeViewHolder>() {
-    private var lastChecked: RadioButton? = null
-    private var lastCheckedPos = 0
+    private var selectCheck: ArrayList<Int> = arrayListOf()
 
     private var groupChangeList = arrayListOf<GroupChangeData>(
         GroupChangeData("그룹미지정"),
@@ -22,6 +19,12 @@ class GroupChangeAdapter : RecyclerView.Adapter<GroupChangeAdapter.GroupChangeVi
         GroupChangeData("전설의 레전드"),
         GroupChangeData("건강챙기자")
     )
+
+    init {
+        for(i in groupChangeList){
+            selectCheck.add(0)
+        }
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -37,13 +40,27 @@ class GroupChangeAdapter : RecyclerView.Adapter<GroupChangeAdapter.GroupChangeVi
         )
 
     override fun onBindViewHolder(holder: GroupChangeViewHolder, position: Int) {
+        holder.onBind(groupChangeList[position])
 
+        holder.radioButton.isChecked = selectCheck[position] == 1
+        holder.radioButton.setOnClickListener {
+            for (k in selectCheck.indices) {
+                if (k == position) {
+                    selectCheck[k] = 1
+                } else {
+                    selectCheck[k] = 0
+                }
+            }
+            notifyDataSetChanged()
+        }
     }
 
     override fun getItemCount(): Int = groupChangeList.size
 
     inner class GroupChangeViewHolder(private val binding: ItemGroupChangeRadioButtonBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        var radioButton = binding.rbGroupName
+
         fun onBind(item: GroupChangeData) {
             binding.rbGroupName.text = item.groupName
         }
